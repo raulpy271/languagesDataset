7c00b592e0f0d02a260f12e6bb0f7dd70ebf18a7
Raul <raulrufino0@gmail.com>
2021-02-08 10:10:47 -0300

    refactor(driver): Use path inside .env to load the driver

--- src/driver.py
+++ src/driver.py
@@ -1,11 +1,21 @@
+from os import environ
+
 from selenium import webdriver
+from dotenv import load_dotenv
 
-from .consts import driver_path, firefox_path
+
+load_dotenv()
 
 
-driver = webdriver.Firefox(
-    firefox_binary=firefox_path,
-    executable_path=driver_path
-)
+firefox_path = environ.get('firefox_path')
+driver_path = environ.get('driver_path')
+
+
+if firefox_path and driver_path:
+    driver = webdriver.Firefox(
+        firefox_binary=firefox_path,
+        executable_path=driver_path)
+else:
+    driver = webdriver.Firefox()
 
 

2c762b5a47ec96fd83b88c718457be2a67583307
Raul <raulrufino0@gmail.com>
2021-02-14 12:49:45 -0300

    feat(technical_details): Add funtions to get paradigms and type systems of the languages

--- src/language_technical_details.py
+++ src/language_technical_details.py
@@ -11,37 +11,60 @@ def get_row_from_infobox_with_an_specific_text(driver, text):
     text = text.lower()
     for row in get_infobox_rows(driver):
         if text in (row.text.lower()):
-            return row
+            row_data = row.find_element_by_css_selector('td')
+            return row_data
+
+
+def get_list_of_type_systems(driver):
+    text_to_search = 'typing'
+    typing_td = (
+        get_row_from_infobox_with_an_specific_text(driver, text_to_search)) 
+    if typing_td:
+        link_elements_with_types = typing_td.find_elements_by_css_selector('a')
+        types = map(
+            lambda element: element.text, link_elements_with_types)
+        return list(types)
+
+
+def get_paradigms(driver):
+    text_to_search = 'paradigm'
+    paradigms_td = (
+        get_row_from_infobox_with_an_specific_text(driver, text_to_search)) 
+    if paradigms_td:
+        link_elements_with_paradigms = (
+            paradigms_td.find_elements_by_css_selector('a'))
+        paradigms = map(
+            lambda element: element.text, link_elements_with_paradigms)
+        return list(paradigms)
 
 
 def get_website(driver):
     text_to_search = 'website'
-    website_tr = (
+    website_td = (
         get_row_from_infobox_with_an_specific_text(driver, text_to_search)) 
-    if website_tr:
-        website_link_element = website_tr.find_element_by_css_selector('a')
+    if website_td:
+        website_link_element = website_td.find_element_by_css_selector('a')
         link_str = website_link_element.get_attribute('href')
         return link_str
 
 
 def get_first_paragraph(driver):
-    first_paragraph = ''
     all_paragraphs = driver.find_elements_by_css_selector(
         'div.mw-parser-output>p')
     for paragraph in all_paragraphs:
         if paragraph.text:
             first_paragraph = paragraph.text
-            break
-    return first_paragraph
+            return first_paragraph
 
 
 def get_details_about_language_from_wiki(driver, language_wiki):
-    language_details = {'link': language_wiki}
+    language_details = {}
     driver.get(language_wiki)
+    link = get_website(driver)
     language_details['about'] = get_first_paragraph(driver)
-    website = get_website(driver)
-    if website:
-        language_details['link'] = website
+    language_details['types'] = get_list_of_type_systems(driver)
+    language_details['paradigms'] = get_paradigms(driver)
+    language_details['link'] = link if link else language_wiki
     return language_details
 
 

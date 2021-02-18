62bcfef7dab1cb5ebaa81a59700d025c3c538a3f
Raul <raulrufino0@gmail.com>
2021-02-18 09:56:34 -0300

    feat(consts): Add path to the dataset with all languages details

--- src/consts.py
+++ src/consts.py
@@ -2,6 +2,7 @@
 
 log_path='/home/raul/Programming/languagesDataset/datasets/'
 path_to_links_dataset = log_path + 'links.tsv'
+path_to_all_languages_dataset = log_path + 'all_languages.tsv'
 link_to_all_languages = (
         'https://en.wikipedia.org/wiki/List_of_programming_languages')
 

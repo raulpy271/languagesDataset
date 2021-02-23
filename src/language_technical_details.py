4a3c2b87e3c9d4140482da7f45a20b7248a66df3
Raul <raulrufino0@gmail.com>
2021-02-23 17:16:27 -0300

    refactor(language_technical_details): Remove some functions
    The functions to remove are these which were added to the finders module. Besides, import these functions from the `finders.py` file.

--- src/language_technical_details.py
+++ src/language_technical_details.py
@@ -1,23 +1,7 @@
 from .driver import get_driver
-
-
-def get_infobox_rows(driver):
-    try:
-        infobox_selector = 'table.infobox>tbody'
-        infobox_table = driver.find_element_by_css_selector(infobox_selector)
-        rows_in_infobox = infobox_table.find_elements_by_css_selector('tr')
-    except:
-        rows_in_infobox = []
-    finally:
-        return rows_in_infobox
-
-
-def get_row_from_infobox_with_an_specific_text(driver, text):
-    text = text.lower()
-    for row in get_infobox_rows(driver):
-        if text in (row.text.lower()):
-            row_data = row.find_element_by_css_selector('td')
-            return row_data
+from .finders import (
+    get_infobox_rows,
+    get_row_from_infobox_with_an_specific_text)
 
 
 def get_list_of_type_systems(driver):

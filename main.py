e70b5e7b6cc5673f09d5ec368edcd04cfa58522d
Raul <raulrufino0@gmail.com>
2021-02-24 08:43:45 -0300

    refactor(main): Add emojis to prints in main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from os import environ
-from os.path import isfile
 
 from pandas import DataFrame, read_csv
 from dotenv import load_dotenv
@@ -22,8 +21,7 @@ def create_links_tsv(driver, path):
 
 
 def get_df_with_links(driver, df_path):
-    if not isfile(df_path):
-        create_links_tsv(driver)
+    create_links_tsv(driver, df_path)
     return read_csv(df_path, sep='\t')
 
 
@@ -37,13 +35,13 @@ def main():
     driver = get_driver()
 
 
-    print('Getting dataset with all language links\n')
+    print('\n⌛ Getting dataset with all language links\n')
     df_with_links = get_df_with_links(driver, path_to_links_dataset)
     df_with_links = use_a_small_df_if_is_test_env(df_with_links)
-    print(f'Dataset with {len(df_with_links)} languages links created\n')
+    print(f'✅ Dataset with {len(df_with_links)} languages links created\n')
 
 
-    print('Getting dataset with all language details')
+    print('⌛ Getting dataset with all language details')
     df_with_details = get_all_languages_details_in_df(
         driver, df_with_links['source'])
     df = df_with_links.merge(
@@ -57,7 +55,7 @@ def main():
 
 if __name__ == '__main__':
     main()
-    print('\nDataset with all language details saved in:',
-          path_to_all_languages_dataset)
+    print('\n✅ Dataset with all language details saved in:',
+          '\n➡️ ', path_to_all_languages_dataset, '\n')
 
 

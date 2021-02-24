4787ee1567102a4f9b70f3939e99f3f89aa9d9e8
Raul <raulrufino0@gmail.com>
2021-02-24 20:26:54 -0300

    perf(datasets): Call function to save memory when importing datasets

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,8 +1,13 @@
-from pandas import read_csv
+import pandas
 
 from src.consts import path_to_all_languages_dataset
+from src.utils import change_types_to_save_memory
 
 
-languages = read_csv(path_to_all_languages_dataset, sep='\t')
+languages_without_memory_saved = (
+    pandas.read_csv(path_to_all_languages_dataset, sep='\t'))
+
+
+languages = change_types_to_save_memory(languages_without_memory_saved, pandas)
 
 
